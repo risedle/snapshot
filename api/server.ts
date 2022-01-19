47ff2b02012af0f68a611f80b1933203c2110895
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-19 23:52:43 +0700

    ✦ Refactor getVaultsYearly data

--- api/server.ts
+++ api/server.ts
@@ -10,6 +10,7 @@ import { getVaultsDaily } from "./getVaultsDaily";
 import { getVaultsWeekly } from "./getVaultsWeekly";
 import { getVaultsMonthly } from "./getVaultsMonthly";
 import { getVaultsThreeMonths } from "./getVaultsThreeMonths";
+import { getVaultsYearly } from "./getVaultsYearly";
 
 // create typeorm connection
 createConnection().then((connection) => {
@@ -69,41 +70,7 @@ createConnection().then((connection) => {
     app.get(
         "/v1/vaults/yearly/:id",
         async function (req: Request, res: Response) {
-            const results = await connection
-                .createQueryBuilder()
-                .select([
-                    "data.timestamp_truncated as timestamp",
-                    "data.borrow_apy",
-                    "data.supply_apy",
-                    "data.utilization_rate",
-                    "data.total_available_cash",
-                    "data.total_outstanding_debt",
-                ])
-                .from((subQuery) => {
-                    return subQuery
-                        .select([
-                            "vaultSnapshot.contractAddress as address",
-                            "vaultSnapshot.borrowAPY as borrow_apy",
-                            "vaultSnapshot.supplyAPY as supply_apy",
-                            "vaultSnapshot.utilizationRate as utilization_rate",
-                            "vaultSnapshot.totalAvailableCash as total_available_cash",
-                            "vaultSnapshot.totalOutstandingDebt as total_outstanding_debt",
-                            "date_trunc('day', vaultSnapshot.timestamp) as timestamp_truncated",
-                            "row_number() over (partition by date_trunc('day', vaultSnapshot.timestamp) order by vaultSnapshot.timestamp desc) as row_number",
-                        ])
-                        .from(VaultSnapshot, "vaultSnapshot")
-                        .where(
-                            "vaultSnapshot.contractAddress = :contractAddress",
-                            {
-                                contractAddress: req.params.id,
-                            }
-                        )
-                        .andWhere(
-                            "vaultSnapshot.timestamp >= NOW() - INTERVAL '1 YEAR'"
-                        );
-                }, "data")
-                .where("data.row_number = :rowNumber", { rowNumber: 1 })
-                .getRawMany();
+            const results = await getVaultsYearly(connection, req.params.id);
             return res.send(results);
         }
     );
