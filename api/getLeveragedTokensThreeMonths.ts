66b3dba8c0a3dd12f958a5fa6ea19f79206b9665
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-01-20 00:06:33 +0700

    ✦ Refactor getLeveragedTokensThreeMonths data

---
+++ api/getLeveragedTokensThreeMonths.ts
@@ -0,0 +1,37 @@
+import { Connection } from "typeorm";
+import { LeveragedTokenSnapshot } from "../entities/LeveragedTokenSnapshot";
+
+export const getLeveragedTokensThreeMonths = async (
+    conn: Connection,
+    leveragedTokenAddress: string
+) => {
+    const results = await conn
+        .createQueryBuilder()
+        .select([
+            "data.timestamp",
+            "data.collateral_per_leveraged_token",
+            "data.debt_per_leveraged_token",
+            "data.leverage_ratio",
+            "data.nav",
+        ])
+        .from((subQuery) => {
+            return subQuery
+                .select([
+                    "snapshot.contractAddress as address",
+                    "snapshot.collateralPerLeveragedToken as collateral_per_leveraged_token",
+                    "snapshot.debtPerLeveragedToken as debt_per_leveraged_token",
+                    "snapshot.leverageRatio as leverage_ratio",
+                    "snapshot.nav as nav",
+                    "date_trunc('hour', timestamp) as timestamp",
+                    "row_number() over (partition by date_trunc('hour', timestamp) order by timestamp desc) as row_number",
+                ])
+                .from(LeveragedTokenSnapshot, "snapshot")
+                .where("snapshot.contractAddress = :contractAddress", {
+                    contractAddress: leveragedTokenAddress,
+                })
+                .andWhere("snapshot.timestamp >= NOW() - INTERVAL '90 DAY'");
+        }, "data")
+        .where("data.row_number = :rowNumber", { rowNumber: 1 })
+        .getRawMany();
+    return results;
+};
