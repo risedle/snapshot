e5ee7b69dacffc84fe8c65508e59d969d2087c47
pyk <bayualdiyansyah@gmail.com>
2022-04-20 01:46:31 +0700

    ✦ Add RiseTokenSnapshot (#29)
    * RiseTokenABI: Fix typo
    
    * Add RiseTokenSnapshot entity
    
    * Add CreateRiseTokenSnapshot migration
    
    * Update docs

---
+++ entities/RiseTokenSnapshot.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+
+@Entity()
+export class RiseTokenSnapshot {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    timestamp: Date;
+
+    @Column()
+    address: string;
+
+    @Column({ type: "float" })
+    cps: number;
+
+    @Column({ type: "float" })
+    dps: number;
+
+    @Column({ type: "float" })
+    lr: number;
+
+    @Column({ type: "float" })
+    nav: number;
+
+    @Column({ type: "float"})
+    totalSupply: number;
+
+    @Column({ type: "float"})
+    totalCollateral: number;
+
+    @Column({ type: "float"})
+    totalDebt: number;
+
+    @Column({ type: "int", nullable: true })
+    blockNumber: number;
+}
